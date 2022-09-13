f5dfb2c3a0e0527786fb2a8e7f1f51bbd5d86091
James Spencer <james.spencer@angel.com>
2022-09-13 13:46:34 -0600

    apply team

--- BasicAds/BasicAds/ViewController.swift
+++ BasicAds/BasicAds/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 import BitmovinPlayer
 
 final class ViewController: UIViewController {
-    let adTagVastSkippable = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="
-    let adTagVast1 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
-    let adTagVast2 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonly&cmsid=496&vid=short_onecue&correlator="
+    let adTagVastSkippable = "https://angel-ad.s3.us-west-2.amazonaws.com/one.xml"
     
     var player: Player!
     
@@ -38,15 +36,11 @@ final class ViewController: UIViewController {
         config.styleConfig.userInterfaceConfig = uiConfig
 
         // Create Advertising configuration
-        let adSource1 = AdSource(tag: urlWithCorrelator(adTag: adTagVastSkippable), ofType: .ima)
-        let adSource2 = AdSource(tag: urlWithCorrelator(adTag: adTagVast1), ofType: .ima)
-        let adSource3 = AdSource(tag: urlWithCorrelator(adTag: adTagVast2), ofType: .ima)
+        let adSource1 = AdSource(tag:  URL(string: adTagVastSkippable)!, ofType: .ima)
         
         let preRoll = AdItem(adSources: [adSource1], atPosition: "pre")
-        let midRoll = AdItem(adSources: [adSource2], atPosition: "20%")
-        let postRoll = AdItem(adSources: [adSource3], atPosition: "post")
         
-        let adConfig = AdvertisingConfig(schedule: [preRoll, midRoll, postRoll])
+        let adConfig = AdvertisingConfig(schedule: [preRoll])
         config.advertisingConfig = adConfig
 
         // Create player based on player configuration
@@ -66,10 +60,6 @@ final class ViewController: UIViewController {
 
         let sourceConfig = SourceConfig(url: streamUrl, type: .hls)
         player.load(sourceConfig: sourceConfig)
-    }
-    
-    func urlWithCorrelator(adTag: String) -> URL {
-        return URL(string: String(format: "%@%d", adTag, Int(arc4random_uniform(100000))))!
     }
 }
 
